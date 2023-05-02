6133882708e6bb979bfa4b2581bf8478f9b19809
wajdem <wajdi.abuarqob@gmail.com>
2023-05-02 20:44:56 +0300

    done

---
+++ js-basic/basic.js
@@ -0,0 +1,130 @@
+// cash flow
+let cash = 1000 , liabilities = 500;
+let cash_flow = cash / liabilities // 1000 / 500 = 2
+console.log(cash_flow)
+
+//net income
+let revenues = 1000 , expenses = 2;
+let net_income = revenues/expenses //1000 / 500 =2
+console.log(net_income)
+
+//Total assets
+liabilities = 1000 , equity = 500;
+total_assets = liabilities + equity //1000 + 500 = 1500
+console.log(total_assets)
+
+//Net income
+let profit = 1000 , sales = 500;
+net_income = profit * sales //1000 * 500
+console.log(net_income)
+
+//Average
+let numbers = 18 , output = 3;
+Average = numbers / output // 18 / 3 = 6
+console.log(Average)
+
+//Discount
+let price = 150 ;
+let discount = 0.3;
+let totalDis = price * discount;
+finalPrice = price - totalDis;
+console.log(finalPrice)
+
+//age limit
+let age = 20;
+if(age > 18){
+    if(age < 30){
+        console.log(true)
+
+    }
+    else{
+        console.log(false)
+    }
+}
+else{
+    console.log(false)
+}
+//Exponential
+let number = 2;
+bower =  3;
+rezelt = number ** bower;
+console.log(rezelt)
+
+//Remainder
+let number_1 = 2;
+number_2 = 4;
+Remainder = number_1 % number_2;
+console.log(Remainder)
+
+
+//task 2
+console.log(typeof(100))
+console.log(typeof(73.9))
+console.log(typeof(NaN))
+console.log(typeof("Water"))
+console.log(typeof(false))
+console.log(typeof(9 != 11))
+console.log("Orang" + "e")
+console.log("Orange" - "s")
+console.log("4" + "8")
+console.log("4" + "8")
+console.log("name" + 3)
+console.log("name" - 3)
+console.log(82 * "word")
+console.log(1 + "hello")
+console.log( "hello" + 1)
+console.log(1 + true)
+console.log("hello" + true)
+console.log(typeof (Infinity))
+console.log(1 == '1')
+console.log(1 === '1')
+//end task 2
+
+
+//string
+
+//string tack 1
+let text = "WELCOME TO ORANGE";
+console.log(text)
+
+let text_1 = "TO";
+console.log(text_1)
+
+let text_2 = "Hello from Orange";
+console.log(text_2)
+
+let text_3 = "welcome to orange";
+console.log(text_3)
+
+let number_3 = 17;
+console.log(number_3)
+
+let text_4 = "Welcome to “Orange”";
+console.log(text_4)
+
+let text_5 = "Welcome to Orange Jordan";
+console.log(text_5)
+//end string tack 1
+
+//string tack 2
+let text_6 = "cactus";
+console.log(
+    text_6.charAt(0) + text_6.slice(1).replace("c" , "*")
+)
+//end string tack 2
+
+//end string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
